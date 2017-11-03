80b96faaa97f8cfc9afb1c74a49cc12d0b9ef8ce
reos44 <amonisal6@gmail.com>
2017-11-03 13:32:18 +0300

    OperatorOverloading added.

---
+++ OperatorOverloading/Gun.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OperatorOverloading
+{
+    public class Gun
+    {
+        public int Capacity { get; set; }
+        public string Name { get; set; }
+        
+        //You must add static and 2 parameter.
+        public static Gun operator +(Gun gun1, Gun gun2)
+        {
+            Gun gun = new Gun();
+            gun.Capacity = gun1.Capacity + gun2.Capacity;
+            gun.Name = gun1.Name + " " + gun2.Name;
+            return gun;
+        }
+    }
+}
---
+++ OperatorOverloadingMain/Program.cs
@@ -0,0 +1,18 @@
+﻿using OperatorOverloading;
+using System;
+
+namespace OperatorOverloadingMain
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Gun gun1 = new Gun() { Name = "M4", Capacity = 40 };
+            Gun gun2 = new Gun() { Name = "M16", Capacity = 30 };
+
+            Gun gun = gun1 + gun2;
+            Console.WriteLine(gun.Name + " " + gun.Capacity);
+            Console.Read();
+        }
+    }
+}
