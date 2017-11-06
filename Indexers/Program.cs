4d496b0e80842d8ab7b910b047f211e8da1d8d1a
TufanOzdemir <amonisal6@gmail.com>
2017-11-06 10:39:13 +0300

    Indexers added.

---
+++ Indexers/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Indexers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack stack = new Stack();
+            stack[0] = 5;
+            stack[1] = 4;
+            stack[2] = 3;
+
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine(stack[i]);
+            }
+            Console.Read();
+        }
+    }
+}
---
+++ Indexers/Stack.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Indexers
+{
+    public class Stack
+    {
+        private int Index { get; set; }
+        private int[] Members { get; set; }
+
+        public Stack()
+        {
+            Members = new int[50];
+            Index = 0;
+        }
+
+        public void Add(int number)
+        {
+            Members[Index] = number;
+            Index++;
+        }
+
+        public int Pop()
+        {
+            Index--;
+            return Members[Index];
+        }
+
+        public int this[int index]
+        {
+            get
+            {
+                if(index < 0 || index > 50)
+                {
+                    return 0;
+                }
+                else
+                {
+                    return Members[index];
+                }
+            }
+            set
+            {
+                if (!(index < 0 || index > 50))
+                {
+                    Members[index] = value;
+                }
+            }
+        }
+    }
+}
