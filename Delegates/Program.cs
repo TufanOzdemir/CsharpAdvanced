06655b247db933f0f979acf74ec521e247195b87
TufanOzdemir <amonisal6@gmail.com>
2017-11-17 11:42:34 +0300

    Delegates added.

---
+++ Delegates/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Delegates
+{
+    class Program
+    {
+        delegate void MyDelegate(int number);
+        static void Main(string[] args)
+        {
+            MyDelegate myDelegate = Number;
+            ExecuteDelegate(5,myDelegate);
+            Console.Read();
+        }
+
+        static void ExecuteDelegate(int number,MyDelegate myDelegate)
+        {
+            myDelegate += Number;
+            myDelegate += Line;
+            myDelegate -= Number;
+            myDelegate(number);
+        }
+
+        static void Number(int number)
+        {
+            Console.WriteLine($"{number} added.");
+        }
+
+        static void Line(int line)
+        {
+            Console.WriteLine($"{line} line added.");
+        }
+    }
+}
