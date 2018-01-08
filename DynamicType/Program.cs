fa004fe3a133ff5d5e805c0112ad23714f56e895
TufanOzdemir <amonisal6@gmail.com>
2018-01-08 12:35:33 +0300

    DynamicType added.

---
+++ DynamicType/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace DynamicType
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Dynamic type convert process
+            Example1();
+            Example2();
+            Console.Read();
+        }
+
+        public static void Example1()
+        {
+            //Error....
+            //string test = "Tufan Ozdemir";
+            //test = 5;
+
+            dynamic test = "Tufan Ozdemir";
+            test = 5;
+
+            Console.WriteLine(test);
+        }
+
+        public static void Example2()
+        {
+            dynamic person = new Person() { Age = 10, Name = "Tufan" };
+            person.Age = 10;
+
+            Console.WriteLine($"Age : {person.Age} Name : {person.Name}");
+        }
+    }
+
+    public class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+}
